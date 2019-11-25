b5e44f9adf8c1ce45a78d9d67a3e01d5b8dc25c1
johnarvi <arvind.john@mail.utoronto.ca>
2019-11-25 13:31:21 -0500

    implemented the usetate application for the daylist item

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -8,7 +8,6 @@ export default function DayListItem(props) {
     "day-list__item--full": props.spots === 0,
   });
   return (
-    // the onclick below sets the whole returned item as clickable
     <li  className={dayClass} onClick={() => props.setDay(props.name)}> 
       <h2 className="text--regular">{props.name}</h2>
       <h3 className="text--light">{formatSpots(props.spots)}</h3>
