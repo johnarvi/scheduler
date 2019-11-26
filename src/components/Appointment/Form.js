87b9cbbb9d0b2f2ac1fd173b89a3682e4a0bd780
johnarvi <arvind.john@mail.utoronto.ca>
2019-11-26 08:20:59 -0500

    implemented the save action still need to pass in props.onCancel and save the value inputed into the field when save button clicked

--- src/components/Appointment/Form.js
+++ src/components/Appointment/Form.js
@@ -11,15 +11,14 @@ export default function Form(props) {
     setInterviewer(null);
   }
 
-  // function cancel() {
-  //   reset();
-  //   props.onCancel;
-  // }
+  function cancel() {
+    reset();
+  }
 
   return (
     <main className="appointment__card appointment__card--create">
       <section className="appointment__card-left">
-        <form autoComplete="off">
+        <form autoComplete="off" onSubmit={event => event.preventDefault()}>
           <input
             className="appointment__create-input text--semi-bold"
             name="name"
@@ -34,7 +33,7 @@ export default function Form(props) {
       <section className="appointment__card-right">
         <section className="appointment__actions">
           <Button danger onClick={reset}>Cancel</Button>
-          <Button confirm>Save</Button>
+          <Button confirm onClick ={props.onSave}>Save</Button>
         </section>
       </section>
     </main>
