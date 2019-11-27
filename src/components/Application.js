63a89e25bb643c221534a8acc7e2679f32d33b1b
johnarvi <arvind.john@mail.utoronto.ca>
2019-11-27 13:53:36 -0500

    implemented the application component to allow for the selector function

--- src/components/Application.js
+++ src/components/Application.js
@@ -3,60 +3,59 @@ import React, { useState, useEffect } from "react";
 import "components/Application.scss";
 import Appointment from "components/Appointment";
 import DayList from "components/DayList";
-const getAppointmentsForDay = require("/home/johnarvi/lighthouse/scheduler/src/helpers/selectors.js");
+const { getAppointmentsForDay } = require("../helpers/selectors.js");
 const axios = require("axios");
 
-const appointments = [
-  {
-    id: 1,
-    time: "12pm",
-  },
-  {
-    id: 2,
-    time: "1pm",
-    interview: {
-      student: "Lydia Miller-Jones",
-      interviewer: {
-        id: 1,
-        name: "Sylvia Palmer",
-        avatar: "https://i.imgur.com/LpaY82x.png",
-      }
-    }
-  },
-  {
-    id: 3,
-    time: "2pm",
-    interview: {
-      student: "Dudes",
-      interviewer: {
-        id: 3,
-        name: "Mildred Nazir",
-        avatar: "https://i.imgur.com/T2WwVfS.png",
-      }
-    }
-  },
-  {
-    id: 4,
-    time: "3pm",
-  },
-  {
-    id: 5,
-    time: "4pm",
-    interview: {
-      student: "another dude",
-      interviewer: {
-        id: 5,
-        name: "Sven Jones", 
-        avatar: "https://i.imgur.com/twYrpay.jpg"
-      }
-    }
-  }
-];
+// const appointments = [
+//   {
+//     id: 1,
+//     time: "12pm",
+//   },
+//   {
+//     id: 2,
+//     time: "1pm",
+//     interview: {
+//       student: "Lydia Miller-Jones",
+//       interviewer: {
+//         id: 1,
+//         name: "Sylvia Palmer",
+//         avatar: "https://i.imgur.com/LpaY82x.png",
+//       }
+//     }
+//   },
+//   {
+//     id: 3,
+//     time: "2pm",
+//     interview: {
+//       student: "Dudes",
+//       interviewer: {
+//         id: 3,
+//         name: "Mildred Nazir",
+//         avatar: "https://i.imgur.com/T2WwVfS.png",
+//       }
+//     }
+//   },
+//   {
+//     id: 4,
+//     time: "3pm",
+//   },
+//   {
+//     id: 5,
+//     time: "4pm",
+//     interview: {
+//       student: "another dude",
+//       interviewer: {
+//         id: 5,
+//         name: "Sven Jones", 
+//         avatar: "https://i.imgur.com/twYrpay.jpg"
+//       }
+//     }
+//   }
+// ];
 
 
 export default function Application(props) {
-  // const [day, setDay] = useState("Monday");
-  // const [days, setDays] = useState([]);
+
   const setDay = day => setState({ ...state, day });
   // const setDays = days => setState(prev => ({ ...prev, days }));
   const [state, setState] = useState({
@@ -64,31 +63,28 @@ export default function Application(props) {
     days: [],
     appointments: {}
   });
-  // const appointments = getAppointmentsForDay(state, state.Day);
-  const appointmentComponents = appointments.map(appointment => {
-    return (
-      <Appointment key={appointment.id} {...appointment} />
-    );
-  });
-
+  
+  // axios doesnt not need another promise. resolve as it already is a promise
   useEffect(() => {
     Promise.all([
-      Promise.resolve(axios.get("/api/days")),
-      Promise.resolve(axios.get("/api/appointments")),
-    ]).then((all) => {
-      setState(prev => ({ days: all[0], appointments: all[1]}));
-      console.log(all[0]);
-      console.log(all[1]);
-    
-      // const [first, second ] = all;
-    
-      // console.log(first, second);
+      axios.get("/api/days"),
+      axios.get("/api/appointments"),
+    ]).then(([days, appointments]) => {
+      setState(prev => {
+        console.log("prev---------------------",prev);
+        return ({ ...prev, days: days.data, appointments: appointments.data})
+      });
     });
-    // axios
-    //   .get("api/days").then((response) => {
-        //setDays(response.data);
-    // })
-  }, []); 
+    }, []);
+
+    console.log(setState);
+    const apps = getAppointmentsForDay(state, state.day);
+    console.log(apps);
+    const appointmentComponents = apps.map(appointment => {
+      return (
+        <Appointment key={appointment.id} {...appointment} />
+      );
+    });
 
   return (
     <main className="layout">
@@ -114,7 +110,7 @@ export default function Application(props) {
       </section>
       <section className="schedule">
         {appointmentComponents}
-        <Appointment key="last" time="5pm" />
+        {/* <Appointment key="last" time="5pm" /> */}
       </section>
     </main>
   );
---
+++ src/helpers/notes.md
@@ -0,0 +1,5 @@
+use state - pass nothing - renders everytime some thing changes, pass an empty array to render once, pass array of values - prerenders when any of the array valus have changed
+
+useEffect - manages side effects - 
+
+use return statement to clean up/ remove a side effect implemented in the function above
