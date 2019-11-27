587af803a63e9f81c481e2a4272de53a87ea6cd7
johnarvi <arvind.john@mail.utoronto.ca>
2019-11-27 08:31:11 -0500

    implemented the set state over set day and setDAys

--- src/components/Application.js
+++ src/components/Application.js
@@ -55,8 +55,16 @@ const appointments = [
 
 
 export default function Application(props) {
-  const [day, setDay] = useState("Monday");
-  const [days, setDays] = useState([]);
+  // const [day, setDay] = useState("Monday");
+  // const [days, setDays] = useState([]);
+  const setDay = day => setState({ ...state, day });
+  const setDays = days => setState(prev => ({ ...prev, days }));
+  const [state, setState] = useState({
+    day: "Monday",
+    days: [],
+    appointments: {}
+  });
+  
   const appointmentComponents = appointments.map(appointment => {
     return (
       <Appointment key={appointment.id} {...appointment} />
@@ -81,8 +89,8 @@ export default function Application(props) {
         <hr className="sidebar__separator sidebar--centered" />
         <nav className="sidebar__menu">
           <DayList
-            days={days}
-            day={day}
+            days={state.days}
+            day={state.day}
             setDay={setDay}
           />
         </nav>
