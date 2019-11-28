118c5cac4c8d0a64a86b5428f7ba276e6ab748d2
johnarvi <arvind.john@mail.utoronto.ca>
2019-11-27 21:31:08 -0500

    fiimplmented the show and create cards etc using custom hooks

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -2,10 +2,12 @@
 // how to call the above in other files
 
 import React from "react";
+import useVisualMode from "hooks/useVisualMode.js"
 
 import Header from "components/Appointment/Header";
 import Empty from "components/Appointment/Empty";
 import Show from "components/Appointment/Show";
+import Form from "components/Appointment/Form";
 
 import "components/Appointment/styles.scss";
 // import Button from "components/Button";
@@ -13,10 +15,32 @@ import "components/Appointment/styles.scss";
 
 
 export default function Appointment(props) {
+
+  const EMPTY = "EMPTY";
+  const SHOW = "SHOW";
+  const CREATE = "CREATE";
+  
+  const { mode, transition, back } = useVisualMode(
+    props.interview ? SHOW : EMPTY
+  );
+
   return (
   <article className="appointment">
     <Header time = {props.time}/>
-    { props.interview ? <Show student={props.interview.student} interviewer={props.interview.interviewer.name}/> : <Empty />}
+    {mode === EMPTY && <Empty onAdd={ () => transition(CREATE)} />}
+    {mode === SHOW && (
+      <Show
+        student={props.interview.student}
+        interviewer={props.interview.interviewer}
+      />
+    )}
+    {mode === CREATE && (
+      <Form
+        interviewers={[]}
+        onCancel={ () => back() }
+        // {props.interview.interviewer}
+      />
+    )}
   </article>
   );
 }
