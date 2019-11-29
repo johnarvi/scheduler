4b28a7434d704b709c187f7172a46e961bc32ce3
johnarvi <arvind.john@mail.utoronto.ca>
2019-11-29 11:24:19 -0500

    implemented the persistence for the interviewer onEdit

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -86,6 +86,7 @@ export default function Appointment(props) {
     {mode === EDIT && (
       <Form
         name = {props.interview && props.interview.student}
+        interviewer={props.interview.interviewer.id}
         interviewers={props.interviewers}
         onCancel={() => back()}
         onSave = {save}
